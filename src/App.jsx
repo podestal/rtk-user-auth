2bd9dd9bbcc244b086589c7864807f0e4072f5db
podestal <luisrodriguezpodesta@gmail.com>
2023-12-21 12:59:12 -0500

    login auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import React from "react"
+import { Routes, Route } from "react-router-dom"
+import Public from "./components/Public"
+import Layout from "./components/Layout"
+import Login from "./features/auth/Login"
+import RequireAuth from "./features/auth/RequireAuth"
+import Welcome from "./features/auth/Welcome"
 
 const App = () => {
 
   return (
-    <>
-      <h1>User Auth</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />} >
+        {/* Public Routes */}
+        <Route index element={<Public />}/>
+        <Route path="login" element={<Login />} />
+        {/* Public Routes */}
+        <Route element={<RequireAuth />}>
+          <Route path="welcome" element={<Welcome />}/>
+        </Route>
+      </Route>
+    </Routes>
   )
 }
 
